db516792149f7fa2ee4ebfacf6a173d5f968ae11
Ivan Sued <thefenry@gmail.com>
2017-12-06 18:07:48 -0800

    Add project files.

---
+++ AdventCalendarOfCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventCalendarOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
